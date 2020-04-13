e372462ab1d717c3a3ced80186aabd807f94bc59
vikash512 <30590266+vikash512@users.noreply.github.com>
2020-04-13 16:45:52 +0530

    Update 001. List Comprehensions.py

--- Python/02. Basic Datatypes/001. List Comprehensions.py
+++ Python/02. Basic Datatypes/001. List Comprehensions.py
@@ -1,3 +1,24 @@
 x, y, z, n = [int(input()) for _ in range(4)]
 listOfAnswers = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n]
 print(listOfAnswers)
+
+
+or
+
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+list_ans= []
+list_ans1 = []
+for i in range (x+1):
+    for j in range (y+1):
+        for k in range (z+1):
+            if (i+j+k)!=n:
+              list_ans.append(i)
+              list_ans.append(j)
+              list_ans.append(k)
+              list_ans1.append([i,j,k])
+print(list_ans1)  
